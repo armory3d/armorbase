e9927f128180381f0ab5d82d16018d2b065fb4ae
Lubos Lenco <lubos.lenco@gmail.com>
2021-07-05 14:45:10 +0200

    Move in base classes

--- Sources/arm/sys/BuildMacros.hx
+++ Sources/arm/sys/BuildMacros.hx
@@ -9,4 +9,13 @@ class BuildMacros {
 		return Context.makeExpr(sys.FileSystem.readDirectory(path), Context.currentPos());
 	}
 	#end
+
+	macro public static function date(): ExprOf<String> {
+		return Context.makeExpr(Date.now().toString(), Context.currentPos());
+	}
+
+	macro public static function sha(): ExprOf<String> {
+		var proc = new sys.io.Process("git", ["log", "--pretty=format:'%h'", "-n", "1"]);
+		return Context.makeExpr(proc.stdout.readLine(), Context.currentPos());
+	}
 }
