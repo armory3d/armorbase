7c29e5bbc1da25273d8e3025b47c324602bb4e48
Lubos Lenco <lubos.lenco@gmail.com>
2021-09-10 15:25:53 +0200

    Separate raytraced bake path

--- Sources/arm/render/RenderPathRaytrace.hx
+++ Sources/arm/render/RenderPathRaytrace.hx
@@ -8,8 +8,6 @@ import iron.Scene;
 class RenderPathRaytrace {
 
 	public static var frame = 0;
-	public static var raysPix = 0;
-	public static var raysSec = 0;
 	public static var ready = false;
 	public static var dirty = 0;
 	static var path: RenderPath;
@@ -19,12 +17,9 @@ class RenderPathRaytrace {
 	static var vb_scale = 1.0;
 	static var vb: kha.graphics4.VertexBuffer;
 	static var ib: kha.graphics4.IndexBuffer;
-	static var raysTimer = 0.0;
-	static var raysCounter = 0;
-	static var lastLayer: kha.Image = null;
+
 	static var lastEnvmap: kha.Image = null;
 	static var isBake = false;
-	static var lastBake = 0;
 
 	#if kha_direct3d12
 	static inline var ext = ".cso";
@@ -93,7 +88,6 @@ class RenderPathRaytrace {
 		f32[20] = Scene.active.world.probe.raw.strength * 1.5;
 		if (!Context.showEnvmap) f32[20] = -f32[20];
 		f32[21] = Context.envmapAngle;
-		f32[22] = Project.layers.length;
 
 		var framebuffer = path.renderTargets.get("buf").image;
 		Krom.raytraceDispatchRays(framebuffer.renderTarget_, f32.buffer);
@@ -104,119 +98,6 @@ class RenderPathRaytrace {
 		Context.rdirty--;
 
 		// Context.ddirty = 1; // _RENDER
-	}
-
-	public static function commandsBake(parsePaintMaterial: ?Bool->Void): Bool {
-		if (!ready || !isBake || lastBake != Context.bakeType) {
-			var rebuild = !(ready && isBake && lastBake != Context.bakeType);
-			lastBake = Context.bakeType;
-			ready = true;
-			isBake = true;
-			lastEnvmap = null;
-			lastLayer = null;
-
-			if (path.renderTargets.get("baketex0") != null) {
-				path.renderTargets.get("baketex0").image.unload();
-				path.renderTargets.get("baketex1").image.unload();
-				path.renderTargets.get("baketex2").image.unload();
-			}
-
-			{
-				var t = new RenderTargetRaw();
-				t.name = "baketex0";
-				t.width = Config.getTextureResX();
-				t.height = Config.getTextureResY();
-				t.format = "RGBA64";
-				path.createRenderTarget(t);
-			}
-			{
-				var t = new RenderTargetRaw();
-				t.name = "baketex1";
-				t.width = Config.getTextureResX();
-				t.height = Config.getTextureResY();
-				t.format = "RGBA64";
-				path.createRenderTarget(t);
-			}
-			{
-				var t = new RenderTargetRaw();
-				t.name = "baketex2";
-				t.width = Config.getTextureResX();
-				t.height = Config.getTextureResY();
-				t.format = "RGBA64"; // Match raytrace_target format
-				path.createRenderTarget(t);
-			}
-
-			var _bakeType = Context.bakeType;
-			Context.bakeType = BakeInit;
-			parsePaintMaterial();
-			path.setTarget("baketex0");
-			path.clearTarget(0x00000000); // Pixels with alpha of 0.0 are skipped during raytracing
-			path.setTarget("baketex0", ["baketex1"]);
-			path.drawMeshes("paint");
-			Context.bakeType = _bakeType;
-			function _next() {
-				parsePaintMaterial();
-			}
-			App.notifyOnNextFrame(_next);
-
-			raytraceInit(getBakeShaderName(), rebuild);
-
-			return false;
-		}
-
-		if (!Context.envmapLoaded) Context.loadEnvmap();
-		var probe = Scene.active.world.probe;
-		var savedEnvmap = Context.showEnvmapBlur ? probe.radianceMipmaps[0] : Context.savedEnvmap;
-		if (lastEnvmap != savedEnvmap || lastLayer != Context.layer.texpaint) {
-			lastEnvmap = savedEnvmap;
-			lastLayer = Context.layer.texpaint;
-
-			var baketex0 = path.renderTargets.get("baketex0").image;
-			var baketex1 = path.renderTargets.get("baketex1").image;
-			var bnoise_sobol = Scene.active.embedded.get("bnoise_sobol.k");
-			var bnoise_scramble = Scene.active.embedded.get("bnoise_scramble.k");
-			var bnoise_rank = Scene.active.embedded.get("bnoise_rank.k");
-			var texpaint_undo = RenderPath.active.renderTargets.get("texpaint_undo" + History.undoI).image;
-			Krom.raytraceSetTextures(baketex0.renderTarget_, baketex1.renderTarget_, texpaint_undo.renderTarget_, savedEnvmap.texture_, bnoise_sobol.texture_, bnoise_scramble.texture_, bnoise_rank.texture_);
-		}
-
-		if (Context.brushTime > 0) {
-			Context.pdirty = 2;
-			Context.rdirty = 2;
-		}
-
-		if (Context.pdirty > 0) {
-			f32[0] = frame++;
-			f32[1] = Context.bakeAoStrength;
-			f32[2] = Context.bakeAoRadius;
-			f32[3] = Context.bakeAoOffset;
-			f32[4] = Scene.active.world.probe.raw.strength;
-			f32[5] = Context.bakeUpAxis;
-			f32[6] = Context.envmapAngle;
-
-			var framebuffer = path.renderTargets.get("baketex2").image;
-			Krom.raytraceDispatchRays(framebuffer.renderTarget_, f32.buffer);
-
-			path.setTarget("texpaint" + Context.layer.id);
-			path.bindTarget("baketex2", "tex");
-			path.drawShader("shader_datas/copy_pass/copy_pass");
-
-			raysPix = frame * 64;
-			raysCounter += 64;
-			raysTimer += iron.system.Time.realDelta;
-			if (raysTimer >= 1) {
-				raysSec = raysCounter;
-				raysTimer = 0;
-				raysCounter = 0;
-			}
-			return true;
-		}
-		else {
-			frame = 0;
-			raysTimer = 0;
-			raysCounter = 0;
-			return false;
-		}
 	}
 
 	static function raytraceInit(shaderName: String, build = true) {
@@ -245,14 +126,6 @@ class RenderPathRaytrace {
 		vb_scale = mo.parent.transform.scale.x * md.scalePos * mo_scale;
 		vb = geom.vertexBuffer;
 		ib = geom.indexBuffers[0];
-	}
-
-	static function getBakeShaderName(): String {
-		return
-			Context.bakeType == BakeAO  		? "raytrace_bake_ao" + ext :
-			Context.bakeType == BakeLightmap 	? "raytrace_bake_light" + ext :
-			Context.bakeType == BakeBentNormal  ? "raytrace_bake_bent" + ext :
-												  "raytrace_bake_thick" + ext;
 	}
 
 	public static function draw(useLiveLayer: Bool) {
