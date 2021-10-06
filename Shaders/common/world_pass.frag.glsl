211aeaf647646fe4a2c9ada925f45161234758ba
Lubos Lenco <lubos.lenco@gmail.com>
2021-10-06 12:19:34 +0200

    Flip world normal

--- Shaders/common/world_pass.frag.glsl
+++ Shaders/common/world_pass.frag.glsl
@@ -11,6 +11,6 @@ out vec4 fragColor;
 void main() {
 	vec4 envmapDataLocal = envmapDataWorld; // TODO: SPIRV workaround
 	vec3 n = normalize(normal);
-	fragColor.rgb = texture(envmap, envMapEquirect(n, envmapDataLocal.x)).rgb * envmapDataLocal.w;
+	fragColor.rgb = texture(envmap, envMapEquirect(-n, envmapDataLocal.x)).rgb * envmapDataLocal.w;
 	fragColor.a = 0.0; // Mark as non-opaque
 }
