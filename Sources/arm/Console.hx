b12bed94daaf033ee52778493d21ca5bee4f6f00
Lubos Lenco <lubos.lenco@gmail.com>
2021-11-03 15:40:36 +0100

    Add console toast

--- Sources/arm/Console.hx
+++ Sources/arm/Console.hx
@@ -11,6 +11,25 @@ class Console {
 	public static var lastTraces: Array<String> = [""];
 	static var haxeTrace: Dynamic->haxe.PosInfos->Void = null;
 
+	public static function toast(s: String) {
+		// Show a popup message
+		function _render(g: kha.graphics2.Graphics) {
+			g.color = 0x55000000;
+			g.fillRect(0, 0, kha.System.windowWidth(), kha.System.windowHeight());
+			var scale = arm.App.getUIs()[0].SCALE();
+			var x = kha.System.windowWidth() / 2;
+			var y = kha.System.windowHeight() - 200 * scale;
+			g.fillRect(x - 200 * scale, y, 400 * scale, 80 * scale);
+			g.font = App.font;
+			g.fontSize = Std.int(22 * scale);
+			g.color = 0xffffffff;
+			g.drawString(s, x - g.font.width(g.fontSize, s) / 2, y + 40 * scale - g.font.height(g.fontSize) / 2);
+			iron.App.removeRender2D(_render);
+		}
+		iron.App.notifyOnRender2D(_render);
+		consoleTrace(s);
+	}
+
 	public static function info(s: String) {
 		messageTimer = 5.0;
 		message = s;
